79cfde16fc8d9669f90466cd826c6f2d4aed5dcf
haedrkeplawe <111405528+haedrkeplawe@users.noreply.github.com>
2026-09-01 02:24:14 +0400

    Update product.controller.js

--- backend-shop/controllers/product.controller.js
+++ backend-shop/controllers/product.controller.js
@@ -1,5 +1,5 @@
-const Product = require("../models/Product");
-const Category = require("../models/Category");
+const Product = require("../models/product");
+const Category = require("../models/category");
 
 const STORE_PREFIX = "TRZ";
 
