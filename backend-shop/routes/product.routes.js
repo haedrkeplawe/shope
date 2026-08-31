23de4adce30b50096debf579447601a27e3c8bd0
haedrkeplawe <111405528+haedrkeplawe@users.noreply.github.com>
2026-09-01 02:21:35 +0400

    Fix uploadProductMedia import case sensitivity

--- backend-shop/routes/product.routes.js
+++ backend-shop/routes/product.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const verifyStore = require("../middleware/verifyStore");
-const uploadProductMedia = require("../middleware/uploadProductMedia");
+const uploadProductMedia = require("../middleware/uploadproductmedia");
 const {
   getProducts,
   getProductById,
