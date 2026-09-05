10f11e033ad0c82e1ff2479ea2c81524cc85f278
haedrkeplawe <111405528+haedrkeplawe@users.noreply.github.com>
2026-09-05 23:20:01 +0400

    Delete frontend-shop-user directory

--- frontend-shop-user/src/utils/formatPrice.js
+++
@@ -1,9 +0,0 @@
-// user
-/*
-  formatPrice
-  - تنسيق السعر بفواصل الآلاف - مستخدمة بأي مكان بيعرض سعر منتج
-*/
-export const formatPrice = (value) => {
-  if (value === null || value === undefined) return "";
-  return Number(value).toLocaleString("en-US");
-};
