75b7039a2b6e54027f1e19055db5318bea2e9acc
haedrkeplawe <111405528+haedrkeplawe@users.noreply.github.com>
2026-09-05 18:58:40 +0400

    Update server.js

--- backend-shop/server.js
+++ backend-shop/server.js
@@ -32,6 +32,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:3001",
       "https://shope-z9xu.onrender.com",
+      "https://shope-user.onrender.com"
     ],
     credentials: true,
   }),
